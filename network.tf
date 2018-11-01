e84f8ac45144c222ac1ead4185b7a4da3e67c41d
guiadco <guiadco@geekhomeinside.com>
2018-11-01 12:24:44 +0100

    Fix CircleCI jobs#1

--- network.tf
+++ network.tf
@@ -5,6 +5,6 @@ resource "google_compute_network" "allspark_network" {
 resource "google_compute_subnetwork" "allspark_network_subnetwork" {
   name          = "${var.network}-subnetwork-${var.subnetwork-region}"
   region        = "${var.subnetwork-region}"
-  network       = "${google_compute_network.${var.network}.self_link}"
+  network       = "${google_compute_network.allspark_network.self_link}"
   ip_cidr_range = "10.0.0.0/16"
 }
